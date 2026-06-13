e7a8fa089bfb9b1ea717ad503e91686aaa0021dd
shulabhsapkota <shulabhsapkota12@gmail.com>
2026-06-13 14:05:07 +0545

    placeholder, image support, etc shits

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const image = document.querySelector(
   ".container .weather img"
 );
 
+input.placeholder =
+  "Enter city name...";
+
 search.addEventListener("click", (e) => {
   input.style.display = "block";
 
@@ -40,6 +43,10 @@ input.addEventListener("keydown", (event) => {
   }
 });
 
+input.addEventListener("focus", () => {
+  input.select();
+});
+
 function formateTime(time) {
   const currentTime =
     new Date(time * 1000);
@@ -68,114 +75,146 @@ let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 async function checkWeather(city) {
-  let response =
-    await fetch(
-      apiUrl +
-        city +
-        `&APPID=${apiKey}`
+  try {
+    let response =
+      await fetch(
+        apiUrl +
+          city +
+          `&APPID=${apiKey}`
+      );
+
+    if (!response.ok) {
+      alert(
+        "City not found!"
+      );
+      return;
+    }
+
+    let data =
+      await response.json();
+
+    console.log(data);
+
+    const date =
+      new Date();
+
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
+    const formattedDate =
+      date.toLocaleDateString(
+        "en-IN",
+        options
+      );
+
+    document.querySelector(
+      ".container .weather .date"
+    ).innerHTML =
+      formattedDate;
+
+    document.querySelector(
+      ".city"
+    ).innerHTML =
+      data.name;
+
+    document.querySelector(
+      ".container .weather .temp"
+    ).innerHTML =
+      data.main.temp + "°c";
+
+    document.querySelector(
+      ".container .weather .condition"
+    ).innerHTML =
+      data.weather[0].main;
+
+    document.querySelector(
+      ".container .details .sun .sunrise .time"
+    ).innerHTML =
+      formateTime(
+        data.sys.sunrise
+      );
+
+    document.querySelector(
+      ".container .details .sun .sunset .time"
+    ).innerHTML =
+      formateTime(
+        data.sys.sunset
+      );
+
+    document.querySelector(
+      ".container .details .atmosphere .pressure .value"
+    ).innerHTML =
+      data.main.pressure;
+
+    document.querySelector(
+      ".container .details .atmosphere .humidity .value"
+    ).innerHTML =
+      data.main.humidity;
+
+    document.querySelector(
+      ".container .details .air .speed .value"
+    ).innerHTML =
+      data.wind.speed;
+
+    document.querySelector(
+      ".container .details .air .degree .value"
+    ).innerHTML =
+      data.wind.deg;
+
+    if (
+      data.weather[0].main ===
+      "Haze"
+    ) {
+      image.src =
+        "clouds.png";
+    } else if (
+      data.weather[0].main ===
+      "Clear"
+    ) {
+      image.src =
+        "clear.png";
+    } else if (
+      data.weather[0].main ===
+      "Rain"
+    ) {
+      image.src =
+        "rain.png";
+    } else if (
+      data.weather[0].main ===
+      "Drizzle"
+    ) {
+      image.src =
+        "drizzle.png";
+    } else if (
+      data.weather[0].main ===
+      "Mist"
+    ) {
+      image.src =
+        "Mist.png";
+    } else {
+      image.src =
+        "clouds.png";
+    }
+
+    input.value = "";
+  } catch (error) {
+    alert(
+      "Network error. Please try again."
     );
 
-  let data =
-    await response.json();
-
-  console.log(data);
-
-  const date = new Date();
-
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-
-  const formattedDate =
-    date.toLocaleDateString(
-      "en-IN",
-      options
-    );
-
-  document.querySelector(
-    ".container .weather .date"
-  ).innerHTML = formattedDate;
-
-  document.querySelector(
-    ".city"
-  ).innerHTML = data.name;
-
-  document.querySelector(
-    ".container .weather .temp"
-  ).innerHTML =
-    data.main.temp + "°c";
-
-  document.querySelector(
-    ".container .weather .condition"
-  ).innerHTML =
-    data.weather[0].main;
-
-  document.querySelector(
-    ".container .details .sun .sunrise .time"
-  ).innerHTML =
-    formateTime(
-      data.sys.sunrise
-    );
-
-  document.querySelector(
-    ".container .details .sun .sunset .time"
-  ).innerHTML =
-    formateTime(
-      data.sys.sunset
-    );
-
-  document.querySelector(
-    ".container .details .atmosphere .pressure .value"
-  ).innerHTML =
-    data.main.pressure;
-
-  document.querySelector(
-    ".container .details .atmosphere .humidity .value"
-  ).innerHTML =
-    data.main.humidity;
-
-  document.querySelector(
-    ".container .details .air .speed .value"
-  ).innerHTML =
-    data.wind.speed;
-
-  document.querySelector(
-    ".container .details .air .degree .value"
-  ).innerHTML =
-    data.wind.deg;
-
-  if (
-    data.weather[0].main ==
-    "Haze"
-  ) {
-    image.src =
-      "clouds.png";
-  } else if (
-    data.weather[0].main ==
-    "Clear"
-  ) {
-    image.src =
-      "clear.png";
-  } else if (
-    data.weather[0].main ==
-    "Rain"
-  ) {
-    image.src =
-      "rain.png";
-  } else if (
-    data.weather[0].main ==
-    "Drizzle"
-  ) {
-    image.src =
-      "drizzle.png";
-  } else if (
-    data.weather[0].main ==
-    "Mist"
-  ) {
-    image.src =
-      "Mist.png";
+    console.error(error);
   }
 }
+
+window.addEventListener(
+  "load",
+  () => {
+    checkWeather(
+      "Kathmandu"
+    );
+  }
+);
